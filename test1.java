6a9c404471397a8a1586c26fa4514f03c04f40ff
ichwanuzaki <dimasichwanu@gmail.com>
2023-04-13 20:12:28 +0700

    fixing method and adding for logic

--- test1.java
+++ test1.java
@@ -1,34 +1,30 @@
 import java.util.Scanner;
 import java.util.InputMismatchException;
 
-class myMethod {
-    public static void main(String[] args) {
-        //scanner
-        Scanner input = new Scanner(System.in);
-        while (true){
-            //handling int input
-            try {
-                System.out.print("Masukkan angka: ");
-                int umur = input.nextInt();
-            //for logic
-            // for (umur == 10; umur++){
-            //     System.out.println(umur)
-            // }
-            //if logic
-            if (umur == 10){
+class Test {
+    //if and for logic
+     static void myMethod (int angka){
+        for (int i = 1; i <= angka; i ++);{
+            if (angka == 10){
                 System.out.println("Sempurna.");
-            } else if (umur > 10) {
+            } else if (angka > 10) {
                 System.out.println("Maaf kamu tidak bisa lebih dari angka 10, silahkan input angka lain.");
             } else {
                 System.out.println("Kamu tidak sempurna.");
-            }
-            }
-            //handling other type data input
-            catch (InputMismatchException e)
-            {
-                input.next();
-                System.out.println("Maaf kamu hanya bisa input angka saja.");
-            }
+            }}
         }
+
+     
+    //scanner + string input handler 
+    public static void main (String[] args) {
+        while (true) try {
+            Scanner input = new Scanner(System.in);
+                    System.out.print("Masukkan angka: ");
+                    int angka = input.nextInt();
+                    myMethod(angka);}
+
+        catch (InputMismatchException e) {
+            System.out.println("Maaf kamu hanya bisa input angka saja.");        
+    }
     }
 }
